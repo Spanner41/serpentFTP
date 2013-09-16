06f9c0d3435c4f9bf88daa7ac002fbccd9d07335
Spanner41 <41spanner@gmail.com>
2013-09-15 22:28:32 -0500

    Add new demo for sftp connection
    Created a command line program that sends and receives a file
    using sftp.

---
+++ demo_sftp.py
@@ -0,0 +1,82 @@
+import base64
+import getpass
+import os
+import socket
+import sys
+import traceback
+
+import paramiko
+
+
+# setup logging
+paramiko.util.log_to_file('demo_sftp.log')
+
+# get hostname
+username = ''
+if len(sys.argv) > 1:
+    hostname = sys.argv[1]
+    if hostname.find('@') >= 0:
+        username, hostname = hostname.split('@')
+else:
+    hostname = raw_input('Hostname: ')
+if len(hostname) == 0:
+    print '*** Hostname required.'
+    sys.exit(1)
+port = 22
+if hostname.find(':') >= 0:
+    hostname, portstr = hostname.split(':')
+    port = int(portstr)
+
+
+# get username
+if username == '':
+    default_username = getpass.getuser()
+    username = raw_input('Username [%s]: ' % default_username)
+    if len(username) == 0:
+        username = default_username
+password = getpass.getpass('Password for %s@%s: ' % (username, hostname))
+
+
+# get host key, if we know one
+hostkeytype = None
+hostkey = None
+try:
+    host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+except IOError:
+    try:
+        # try ~/ssh/ too, because windows can't have a folder named ~/.ssh/
+        host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/ssh/known_hosts'))
+    except IOError:
+        print '*** Unable to open host keys file'
+        host_keys = {}
+
+if host_keys.has_key(hostname):
+    hostkeytype = host_keys[hostname].keys()[0]
+    hostkey = host_keys[hostname][hostkeytype]
+    print 'Using host key of type %s' % hostkeytype
+
+
+# now, connect and use paramiko Transport to negotiate SSH2 across the connection
+try:
+    t = paramiko.Transport((hostname, port))
+    t.connect(username=username, password=password, hostkey=hostkey)
+    sftp = paramiko.SFTPClient.from_transport(t)
+
+	# Send photo.jpg to server
+	data = open('photo.jpg', 'rb').read()
+    sftp.open('photo.jpg', 'wb').write(data)
+	
+    # Retrieve photo.jpg from server and name it photo-copy.jpg
+    data = sftp.open('photo.jpg', 'rb').read()
+    open('photo-copy.jpg', 'wb').write(data)
+
+    t.close()
+
+except Exception, e:
+    print '*** Caught exception: %s: %s' % (e.__class__, e)
+    traceback.print_exc()
+    try:
+        t.close()
+    except:
+        pass
+    sys.exit(1)
