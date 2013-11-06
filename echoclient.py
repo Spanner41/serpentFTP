269c11ca5d233b4c8f78897c68e5dacd71493d80
Spanner41 <41spanner@gmail.com>
2013-11-05 20:19:06 -0600

    Added simple echo client and server for testing purposes

---
+++ echoclient.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+def listen(client):
+    while 1:
+        try:
+            client.send(raw_input())
+        except Exception, e:
+            exit()
+
+host = '127.0.0.1' 
+port = 50000
+size = 1024 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.settimeout(3.0)
+s.connect((host, port))
+
+sendthread = threading.Thread(target=listen, args = (s, ))
+sendthread.start()
+
+while threading.activeCount() > 1:
+    try:
+        data = s.recv(size)
+        print(data)
+    except socket.timeout:
+        pass
+    except:
+        exit()
+        
+s.close()
---
+++ echoserver.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+def listen(client):
+    while 1:
+        try:
+            client.send(raw_input())
+        except Exception, e:
+            exit()
+
+host = '' 
+port = 50000 
+backlog = 5 
+size = 1024 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((host,port))
+s.listen(backlog)
+
+client, address = s.accept()
+client.settimeout(3.0)
+sendthread = threading.Thread(target=listen, args = (client, ))
+sendthread.start()
+
+while threading.activeCount():
+    try:
+        data = client.recv(size)
+        print(data)
+    except socket.timeout:
+        pass
+    except:
+        exit()
+
+client.close()
+s.close()
